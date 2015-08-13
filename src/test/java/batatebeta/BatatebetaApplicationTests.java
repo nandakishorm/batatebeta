f0d93b0628cdedf7ece4c28326057eed17fa32e0
Nandakishor <nanda.kishor@mmpsd.com>
2015-08-13 21:30:35 +0530

    Batate beta first commit

---
+++ src/test/java/batatebeta/BatatebetaApplicationTests.java
@@ -0,0 +1,18 @@
+package batatebeta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BatatebetaApplication.class)
+@WebAppConfiguration
+public class BatatebetaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
