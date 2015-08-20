e360c6f0fd23627ab7c47c7c4e844aa3ce8141f6
Nandakishor <nanda.kishor@mmpsd.com>
2015-08-20 21:38:20 +0530

    Added Spring security dependency to pom.xml. Implemented UserDetailsService as BatateUserDetailsServiceImpl.java for handling Security filtering. Added SecurityConfig which extends WebSecurityConfigurerAdapter class.   1) Added configure method to set - batateUserDetailsServiceImpl   2) Added configure method to configure HttpSecurity.

---
+++ src/main/java/com/kishor/batatebeta/auth/BatateUserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kishor.batatebeta.auth;
+
+import com.kishor.batatebeta.core.domain.User;
+import com.kishor.batatebeta.core.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Nandakishor on 8/20/2015.
+ */
+@Service("batateUserDetailsServiceImpl")
+public class BatateUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = authenticationService.findByUsername(username);
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+
+        org.springframework.security.core.userdetails.User userDetails =
+                new org.springframework.security.core.userdetails.User(username, user.getPassword(), true, true, true, true, authorities);
+
+        return userDetails;
+    }
+}
---
+++ src/main/java/com/kishor/batatebeta/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.kishor.batatebeta.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by Nandakishor on 8/20/2015.
+ */
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ComponentScan(basePackages = "com.kishor.batatebeta.auth", scopedProxy = ScopedProxyMode.INTERFACES)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("batateUserDetailsServiceImpl")
+    private UserDetailsService userDetailsService;
+
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService);
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated();
+        httpSecurity.httpBasic();
+        httpSecurity.csrf().disable();
+    }
+}
