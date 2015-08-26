ecdb5f552fe8f6021634c63bfeb0aaf00215d198
Nandakishor <nanda.kishor@mmpsd.com>
2015-08-26 20:31:40 +0530

    Added a controller method to invalidate the session, so that the next time when the UI hits the service, BatateUserDetailsServiceImpl will be called.

--- src/main/java/com/kishor/batatebeta/rest/controller/UserController.java
+++ src/main/java/com/kishor/batatebeta/rest/controller/UserController.java
@@ -13,11 +13,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -71,5 +70,13 @@ public class UserController {
         User user = userService.findByUsername(userDetails.getUsername());
         UserResource userResource = userResourceAssembler.toResource(user);
         return new ResponseEntity<>(userResource, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/user/logout", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if(session != null)
+            session.invalidate();
     }
 }
